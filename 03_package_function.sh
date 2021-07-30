f0266a2886f9fe9b4a092c2dda9493fa012635a0
Max Werner <mwerner@obsessiveanalytics.com>
2021-07-30 15:16:37 -0300

    fixing typo

--- 03_package_function.sh
+++ 03_package_function.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mkdir -p dist && zip -FS -q -r ./dist/gpx_webhook_processor.zip node_modules index.js package.json package-lock.json
+mkdir -p dist && zip -FS -q -r ./dist/webhook_processor.zip node_modules index.js package.json package-lock.json
